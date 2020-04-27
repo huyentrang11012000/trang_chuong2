4e7ff91f891e7dd9ff85911670e758d2837f093d
gitexample <huyentrangdoan1101@gmail.com>
2020-04-27 22:36:21 +0700

    commit2

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace chuong2_bt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TamGiac1 tg = new TamGiac1();
+            //tg.nhap();
+            if(tg.istamgiac())
+            {
+                Console.WriteLine("ket qua chu vi tg 1: " + tg.chuvi());
+                Console.WriteLine("ket qua dien tich tg 1: " + tg.dientich());
+            }   
+            Console.ReadLine();
+            TamGiac1 tg2 = new TamGiac1(2,4,3);
+            Console.WriteLine("ket qua chu vi tg 2: " + tg2.chuvi());
+            Console.WriteLine("ket qua dien tich tg 2: " + tg2.dientich());
+             Console.ReadLine();  
+            TamGiac1 tg3 = new TamGiac1(tg2);
+            Console.WriteLine("ket qua tg 3: " + tg3.chuvi());
+            Console.WriteLine("ket qua tg 3: " + tg3.dientich());
+            tg3.nhap();
+            //Console.WriteLine
+            Console.WriteLine("ket qua 3: " + tg3.chuvi());
+            Console.WriteLine("ket qua 3: " + tg3.dientich());
+            Console.WriteLine("ket qua chu vi tg 2 sau khi thay doi: " + tg2.chuvi());
+            Console.WriteLine("ket qua dien tich sau khi thay doi: " + tg2.dientich());
+            Console.Read();
+        }
+        
+
+        
+    }
+}
+
---
+++ TamGiac1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chuong2_bt1
+{
+    class TamGiac1
+    {
+        int a, b, c;
+        public bool istamgiac()
+        { bool ketqua = false;
+            if ((a + b) > c && (a + c) > b && (b + c) > a)
+                ketqua = true;
+            return ketqua;
+
+        }
+        public double chuvi()
+        {
+            if (istamgiac())
+            { 
+                return a + b + c;
+            }  
+         else Console.WriteLine(" day khong phai la tam giac");
+            return (-1);
+        }
+        public double dientich()
+        {
+            if (istamgiac())
+            {double p = 0.5*(a + b + c);
+                return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+            else Console.WriteLine(" day khong phai la tam giac");
+            return (-1);
+
+        }
+        public void nhap()
+        {
+            Console.WriteLine(" hay nhap canh a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" hay nhap canh b");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("hay nhap canh c");
+            c = Convert.ToInt32(Console.ReadLine());
+        }
+        public TamGiac1()
+        {
+            a = b = c = 1;
+        }
+        public TamGiac1( int chieudaicanh)
+        {
+            a = b = c = chieudaicanh;
+        }
+        public TamGiac1( int a, int b, int c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public TamGiac1 (TamGiac1 tg)
+        {
+            this.a = tg.a;
+            this.b = tg.b;
+            this.c = tg.c;
+        }
+
+    }
+}
